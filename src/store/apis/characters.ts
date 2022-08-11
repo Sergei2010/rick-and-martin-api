9272028c78b891bb0de34030e354272c202ec172
sergei2010 <sergejmatvejcuk323@gmail.com>
2022-08-11 15:39:57 +0200

    ready for deploy

--- src/store/apis/characters.ts
+++ src/store/apis/characters.ts
@@ -1,32 +1,32 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
 import { API_URL } from '../../constants';
 import { setPage, setTotalCount } from '../slices/filter';
 import { RootState } from '../store';
-
-export const charactersApi = createApi({
+export const charactersApi = createApi( {
   reducerPath: 'chractersApi',
-  baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
+  baseQuery: fetchBaseQuery( { baseUrl: API_URL } ),
 
-  endpoints: (builder) => ({
-    getAllCharacters: builder.query<ResponseDetails<Character[]>, void>({
-      queryFn: async (arg, queryApi, extraOptions, baseQuery) => {
+  endpoints: ( builder ) => ( {
+    getAllCharacters: builder.query<ResponseDetails<Character[]>, void>( {
+      queryFn: async ( arg, queryApi, extraOptions, baseQuery ) => {
         const state = queryApi.getState();
-        const { searchValue, page, totalCount } = (state as RootState).filter;
+        const { searchValue, page, totalCount } = ( state as RootState ).filter;
+        const response = ( await baseQuery( `/character?name=${ searchValue }&page=${ page }` ) ) as any;
 
-        const response = (await baseQuery(`/character?name=${searchValue}&page=${page}`)) as any;
-
-        if (response.data && response.data.info.count !== totalCount) {
-          queryApi.dispatch(setTotalCount(response.data.info.count));
-          queryApi.dispatch(setPage(1));
+        if ( response.data && response.data.info.count !== totalCount ) {
+          queryApi.dispatch( setTotalCount( response.data.info.count ) );
+          queryApi.dispatch( setPage( 1 ) );
         }
+
         return response;
       },
-    }),
+    } ),
 
-    getCharacterById: builder.query<Character, number>({
-      query: (id) => `/character/${id}`,
-    }),
-  }),
-});
-
+    getCharacterById: builder.query<Character, number>( {
+      query: ( id ) => `/character/${ id }`,
+    } ),
+  } ),
+} );
+//console.log('charactersApi:', charactersApi);
 export const { useGetAllCharactersQuery, useGetCharacterByIdQuery } = charactersApi;
